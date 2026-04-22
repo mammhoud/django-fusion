9b504b721c4f8b6e9880be3c31b795101848413d
$GIT_USERNAME <$GIT_EMAIL>
2026-04-22 13:55:21 +0000

    fix: SiteMiddleware path pipelines.middlewares -> django_osoul.middlewares

--- configs/base/middlewares.py
+++ configs/base/middlewares.py
@@ -21,6 +21,6 @@ MIDDLEWARE = [
     # "allauth.usersessions.middleware.UserSessionsMiddleware",
     # "src.utils.middlewares.htmx.HtmxMiddleware",
     # "django_htmx.middleware.HtmxMiddleware",
-    "django_rseal.pipelines.middlewares.SiteMiddleware",
+    "django_osoul.middlewares.SiteMiddleware",
     # "core.middlewares.language.DefaultLanguageMiddleware",
 ]
