cf669727a452cb4024cecae5bdafa3d288aaebd2
$GIT_USERNAME <$GIT_EMAIL>
2026-05-30 12:36:22 +0000

    fix: reorder import statements in settings.py for clarity

--- structa.cloud/settings.py
+++ structa.cloud/settings.py
@@ -2,6 +2,10 @@
 
 from configs.site import configure_site_environment
 
+from configs.settings import *  # noqa: E402,F401,F403
+
 configure_site_environment("structa.cloud", module="CMS", default_port=5071)
 
-from configs.settings import *  # noqa: E402,F401,F403
+ROOT_URLCONF = "www.core.urls"
+WSGI_APPLICATION = "www.wsgi.application"
+ASGI_APPLICATION = "www.asgi.application"
